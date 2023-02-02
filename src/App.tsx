a95ef1c8956c1b0e5a5bdb914945811269b02f7a
Dmitry Latyshev <dislaty@mail.ru>
2023-02-02 19:27:50 +0300

    feat: add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { AboutLazy } from "./pages/about/About.lazy";
 import { MainLazy } from "./pages/main/Main.lazy";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app light`}>
-      {/* <button onClick={toggleTheme}>TOGGLE</button> */}
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
+
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutLazy />} />
