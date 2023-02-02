68530867bf78cca2295d64ea7deb6643a59c4708
Dmitry Latyshev <dislaty@mail.ru>
2023-02-02 19:10:47 +0300

    feat: add styles folder and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutLazy } from "./pages/about/About.lazy";
 import { MainLazy } from "./pages/main/Main.lazy";
 
 export const App = () => {
   return (
-    <>
-      <Link to="/">Main Page</Link>
-      <Link to="/about">About</Link>
-
-      <Suspense fallback="Loading...">
+    <div className={`app light`}>
+      {/* <button onClick={toggleTheme}>TOGGLE</button> */}
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/"} element={<MainLazy />} />
           <Route path={"/about"} element={<AboutLazy />} />
+          <Route path={"/"} element={<MainLazy />} />
         </Routes>
       </Suspense>
-    </>
+    </div>
   );
 };
