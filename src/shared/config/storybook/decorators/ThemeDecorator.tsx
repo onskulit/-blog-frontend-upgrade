22fce99a401d24630c04f93319d5b91fd74f8459
dislaty <dislaty@mail.ru>
2023-03-02 10:11:27 +0300

    feat: add ThemeProvider in Decorator

--- src/shared/config/storybook/decorators/ThemeDecorator.tsx
+++ src/shared/config/storybook/decorators/ThemeDecorator.tsx
@@ -1,11 +1,13 @@
 import { Story } from '@storybook/react';
-import { Theme } from 'shared/lib/providers/ThemeProvider';
+import { Theme, ThemeProvider } from 'shared/lib/providers/ThemeProvider';
 
 // TODO пришлось назвать тему light -> root.
 export const ThemeDecorator = (theme: Theme) => (Story: Story) => {
     return (
-        <div className={`app ${theme}`}>
-            <Story />
-        </div>
+        <ThemeProvider>
+            <div className={`app ${theme}`}>
+                <Story />
+            </div>
+        </ThemeProvider>
     );
 };
